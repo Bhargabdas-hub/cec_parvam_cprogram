f658aa073afc3d89cb724e8878b4daffe4730771
unknown <bhargabdas0453@gmail.com>
2025-11-26 14:05:31 +0530

    add

---
+++ day 4/new.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int age = 19;
+    printf("your age is:%d years",age);
+    return 0;
+}
---
+++ day 5/replace.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={10, 20, 30, 40, 50};
+    int pos=1;
+    int newvalue=99;
+    arr[pos]=newvalue;
+    printf("array after update:\n");
+    for (int i=0;i<5;i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
